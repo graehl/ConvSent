d8481d47486fdb78d5b7d3a0bc226627b8ee73fd
graehl <graehl@gmail.com>
2017-11-06 12:31:36 -0800

    theano gpuarray backend

--- sentence_retrieval.py
+++ sentence_retrieval.py
@@ -1,5 +1,5 @@
 '''
-Learning Generic Sentence Representations Using Convolutional Neural Networks 
+Learning Generic Sentence Representations Using Convolutional Neural Networks
 https://arxiv.org/pdf/1611.07897.pdf
 Developed by Zhe Gan, zhe.gan@duke.edu, April, 19, 2016
 '''
@@ -18,9 +18,9 @@ from scipy import spatial
 
 
 def prepare_data_for_cnn(seqs_x, maxlen=40, n_words=21103, filter_h=5):
-    
+
     lengths_x = [len(s) for s in seqs_x]
-    
+
     if maxlen != None:
         new_seqs_x = []
         new_lengths_x = []
@@ -30,13 +30,13 @@ def prepare_data_for_cnn(seqs_x, maxlen=40, n_words=21103, filter_h=5):
                 new_lengths_x.append(l_x)
         lengths_x = new_lengths_x
         seqs_x = new_seqs_x
-        
+
         if len(lengths_x) < 1  :
             return None, None
-    
+
     pad = filter_h -1
-    x = []   
-    for rev in seqs_x:    
+    x = []
+    for rev in seqs_x:
         xx = []
         for i in xrange(pad):
             xx.append(n_words-1)
@@ -49,9 +49,9 @@ def prepare_data_for_cnn(seqs_x, maxlen=40, n_words=21103, filter_h=5):
     return x
 
 def prepare_data_for_rnn(seqs_x, maxlen=40):
-    
+
     lengths_x = [len(s) for s in seqs_x]
-    
+
     if maxlen != None:
         new_seqs_x = []
         new_lengths_x = []
@@ -61,10 +61,10 @@ def prepare_data_for_rnn(seqs_x, maxlen=40):
                 new_lengths_x.append(l_x)
         lengths_x = new_lengths_x
         seqs_x = new_seqs_x
-        
+
         if len(lengths_x) < 1  :
             return None, None
-    
+
     n_samples = len(seqs_x)
     maxlen_x = np.max(lengths_x)
 
@@ -76,7 +76,7 @@ def prepare_data_for_rnn(seqs_x, maxlen=40):
 
     return x, x_mask
 
-def find_sent_embedding(whole, n_words=21102, img_w=300, img_h=48, feature_maps=200, 
+def find_sent_embedding(whole, n_words=21102, img_w=300, img_h=48, feature_maps=200,
     filter_hs=[3,4,5],n_x=300, n_h=600):
 
     options = {}
@@ -87,112 +87,96 @@ def find_sent_embedding(whole, n_words=21102, img_w=300, img_h=48, feature_maps=
     options['filter_hs'] = filter_hs
     options['n_x'] = n_x
     options['n_h'] = n_h
-    
+
     filter_w = img_w
     filter_shapes = []
     pool_sizes = []
     for filter_h in filter_hs:
         filter_shapes.append((feature_maps, 1, filter_h, filter_w))
         pool_sizes.append((img_h-filter_h+1, img_w-filter_w+1))
-        
+
     options['filter_shapes'] = filter_shapes
     options['pool_sizes'] = pool_sizes
-    
+
     params = init_params(options)
     tparams = init_tparams(params)
-    
-    data = np.load('./bookcorpus_result.npz')  
-    
+
+    data = np.load('./bookcorpus_result.npz')
+
     for kk, pp in params.iteritems():
         params[kk] = data[kk]
-    
+
     for kk, pp in params.iteritems():
         tparams[kk].set_value(params[kk])
 
     x = tensor.matrix('x', dtype='int32')
-    
-    layer0_input = tparams['Wemb'][tensor.cast(x.flatten(),dtype='int32')].reshape((x.shape[0],1,x.shape[1],tparams['Wemb'].shape[1])) 
- 
+
+    layer0_input = tparams['Wemb'][tensor.cast(x.flatten(),dtype='int32')].reshape((x.shape[0],1,x.shape[1],tparams['Wemb'].shape[1]))
+
     layer1_inputs = []
     for i in xrange(len(options['filter_hs'])):
         filter_shape = options['filter_shapes'][i]
         pool_size = options['pool_sizes'][i]
-        conv_layer = encoder(tparams, layer0_input,filter_shape, pool_size,prefix=_p('cnn_encoder',i))                          
+        conv_layer = encoder(tparams, layer0_input,filter_shape, pool_size,prefix=_p('cnn_encoder',i))
         layer1_input = conv_layer
         layer1_inputs.append(layer1_input)
     layer1_input = tensor.concatenate(layer1_inputs,1)
-                                 
+
     f_embed = theano.function([x], layer1_input, name='f_embed')
-    
+
     kf = get_minibatches_idx(len(whole), 100)
     sent_emb = np.zeros((len(whole),600))
-    
+
     for i, train_index in kf:
         sents = [whole[t] for t in train_index]
         x = prepare_data_for_cnn(sents)
         sent_emb[train_index[0]:train_index[-1]+1] = f_embed(x)
         if i % 500 == 0:
             print i,
-    
+
     np.savez('./bookcorpus_embedding.npz', sent_emb=sent_emb)
-    
+
     return sent_emb
-        
+
 
 if __name__ == '__main__':
-    
+
     x = cPickle.load(open("./data/bookcorpus_1M.p","rb"))
     train, val, test = x[0], x[1], x[2]
     train_text, val_text, test_text = x[3], x[4], x[5]
     wordtoix, ixtoword = x[6], x[7]
     del x
-    
+
     n_words = len(ixtoword)
-    
+
     ixtoword[n_words] = '<pad_zero>'
     wordtoix['<pad_zero>'] = n_words
     n_words = n_words + 1
-    
+
     whole = train + val + test
     whole_text = train_text + val_text + test_text
     del train, val, test
     del train_text, val_text, test_text
-    
+
     sent_emb = find_sent_embedding(whole)
-    
+
     """ sentence retrieval """
     x = np.load('./bookcorpus_embedding.npz')
     sent_emb = x['sent_emb']
-    
-    idx = 0
-    print whole_text[idx]
+
+    idx = 5
+    print("%s: %s\n"%(idx, whole_text[idx]))
     target_emb = sent_emb[idx]
-    
+
     cos_similarity = []
     for i in range(len(whole)):
         vector = sent_emb[i]
         result = 1 - spatial.distance.cosine(target_emb, vector)
         cos_similarity.append(result)
     top_indices = np.argsort(cos_similarity)[::-1]
-    
+
     print whole_text[top_indices[0]], cos_similarity[top_indices[0]]
     print whole_text[top_indices[1]], cos_similarity[top_indices[1]]
     print whole_text[top_indices[2]], cos_similarity[top_indices[2]]
     print whole_text[top_indices[3]], cos_similarity[top_indices[3]]
     print whole_text[top_indices[4]], cos_similarity[top_indices[4]]
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
--- train_autoencoder.py
+++ train_autoencoder.py
@@ -1,5 +1,5 @@
 '''
-Learning Generic Sentence Representations Using Convolutional Neural Networks 
+Learning Generic Sentence Representations Using Convolutional Neural Networks
 https://arxiv.org/pdf/1611.07897.pdf
 Developed by Zhe Gan, zhe.gan@duke.edu, April, 19, 2016
 '''
@@ -22,9 +22,9 @@ from model.utils import get_minibatches_idx, unzip
 #theano.config.compute_test_value = 'warn'
 
 def prepare_data_for_cnn(seqs_x, maxlen=40, n_words=21103, filter_h=5):
-    
+
     lengths_x = [len(s) for s in seqs_x]
-    
+
     if maxlen != None:
         new_seqs_x = []
         new_lengths_x = []
@@ -34,13 +34,13 @@ def prepare_data_for_cnn(seqs_x, maxlen=40, n_words=21103, filter_h=5):
                 new_lengths_x.append(l_x)
         lengths_x = new_lengths_x
         seqs_x = new_seqs_x
-        
+
         if len(lengths_x) < 1  :
             return None, None
-    
+
     pad = filter_h -1
-    x = []   
-    for rev in seqs_x:    
+    x = []
+    for rev in seqs_x:
         xx = []
         for i in xrange(pad):
             # we need pad the special <pad_zero> token.
@@ -54,9 +54,9 @@ def prepare_data_for_cnn(seqs_x, maxlen=40, n_words=21103, filter_h=5):
     return x
 
 def prepare_data_for_rnn(seqs_x, maxlen=40):
-    
+
     lengths_x = [len(s) for s in seqs_x]
-    
+
     if maxlen != None:
         new_seqs_x = []
         new_lengths_x = []
@@ -66,10 +66,10 @@ def prepare_data_for_rnn(seqs_x, maxlen=40):
                 new_lengths_x.append(l_x)
         lengths_x = new_lengths_x
         seqs_x = new_seqs_x
-        
+
         if len(lengths_x) < 1  :
             return None, None
-    
+
     n_samples = len(seqs_x)
     maxlen_x = np.max(lengths_x)
 
@@ -80,15 +80,15 @@ def prepare_data_for_rnn(seqs_x, maxlen=40):
         x_mask[:lengths_x[idx], idx] = 1.
 
     return x, x_mask
-    
+
 def calu_cost(f_cost, prepare_data_for_cnn, prepare_data_for_rnn, data, kf):
 
     total_negll = 0.
     total_len = 0.
-    
+
     for _, train_index in kf:
         sents = [train[t]for t in train_index]
-                
+
         x = prepare_data_for_cnn(sents)
         y, y_mask = prepare_data_for_rnn(sents)
         negll = f_cost(x, y, y_mask) * np.sum(y_mask)
@@ -100,19 +100,19 @@ def calu_cost(f_cost, prepare_data_for_cnn, prepare_data_for_rnn, data, kf):
 
 """ Training the model. """
 
-def train_model(train, val, test, n_words=21103, img_w=300, max_len=40, 
-    feature_maps=200, filter_hs=[3,4,5], n_x=300, n_h=600, 
-    max_epochs=8, lrate=0.0002, batch_size=64, valid_batch_size=64, dispFreq=10, 
+def train_model(train, val, test, n_words=21103, img_w=300, max_len=40,
+    feature_maps=200, filter_hs=[3,4,5], n_x=300, n_h=600,
+    max_epochs=8, lrate=0.0002, batch_size=64, valid_batch_size=64, dispFreq=10,
     validFreq=500, saveFreq=1000, saveto = 'bookcorpus_result.npz'):
-        
+
     """ train, valid, test : datasets
         n_words : vocabulary size
         img_w : word embedding dimension, must be 300.
-        max_len : the maximum length of a sentence 
-        feature_maps : the number of feature maps we used 
+        max_len : the maximum length of a sentence
+        feature_maps : the number of feature maps we used
         filter_hs: the filter window sizes we used
         n_x: word embedding dimension
-        n_h: the number of hidden units in LSTM        
+        n_h: the number of hidden units in LSTM
         max_epochs : the maximum number of epoch to run
         lrate : learning rate
         batch_size : batch size during training
@@ -122,9 +122,9 @@ def train_model(train, val, test, n_words=21103, img_w=300, max_len=40,
         saveFreq: save the result after this number of update.
         saveto: where to save the result.
     """
-    
+
     img_h = max_len + 2*(filter_hs[-1]-1)
-    
+
     options = {}
     options['n_words'] = n_words
     options['img_w'] = img_w
@@ -140,55 +140,55 @@ def train_model(train, val, test, n_words=21103, img_w=300, max_len=40,
     options['dispFreq'] = dispFreq
     options['validFreq'] = validFreq
     options['saveFreq'] = saveFreq
-   
+
     logger.info('Model options {}'.format(options))
 
     logger.info('Building model...')
-    
+
     filter_w = img_w
     filter_shapes = []
     pool_sizes = []
     for filter_h in filter_hs:
         filter_shapes.append((feature_maps, 1, filter_h, filter_w))
         pool_sizes.append((img_h-filter_h+1, img_w-filter_w+1))
-        
+
     options['filter_shapes'] = filter_shapes
     options['pool_sizes'] = pool_sizes
-    
+
     params = init_params(options)
     tparams = init_tparams(params)
 
     use_noise, x, y, y_mask, cost = build_model(tparams,options)
-    
+
     f_cost = theano.function([x, y, y_mask], cost, name='f_cost')
-    
+
     lr = tensor.scalar(name='lr')
     f_grad_shared, f_update = Adam(tparams, cost, [x, y, y_mask], lr)
-    
+
     logger.info('Training model...')
-    
-    history_cost = []  
+
+    history_cost = []
     uidx = 0  # the number of update done
     start_time = time.time()
-    
+
     kf_valid = get_minibatches_idx(len(val), valid_batch_size)
-    
+
     zero_vec_tensor = tensor.vector()
     zero_vec = np.zeros(img_w).astype(theano.config.floatX)
     set_zero = theano.function([zero_vec_tensor], updates=[(tparams['Wemb'], tensor.set_subtensor(tparams['Wemb'][21102,:], zero_vec_tensor))])
-    
+
     try:
         for eidx in xrange(max_epochs):
             n_samples = 0
-            
+
             kf = get_minibatches_idx(len(train), batch_size, shuffle=True)
 
             for _, train_index in kf:
                 uidx += 1
                 use_noise.set_value(0.)
 
-                sents = [train[t]for t in train_index]
-                
+                sents = [train[t] for t in train_index]
+
                 x = prepare_data_for_cnn(sents)
                 y, y_mask = prepare_data_for_rnn(sents)
                 n_samples += y.shape[1]
@@ -199,28 +199,28 @@ def train_model(train, val, test, n_words=21103, img_w=300, max_len=40,
                 set_zero(zero_vec)
 
                 if np.isnan(cost) or np.isinf(cost):
-                    
+
                     logger.info('NaN detected')
                     return 1., 1., 1.
 
                 if np.mod(uidx, dispFreq) == 0:
                     logger.info('Epoch {} Update {} Cost {}'.format(eidx, uidx, np.exp(cost)))
-                
+
                 if np.mod(uidx, saveFreq) == 0:
-                    
+
                     logger.info('Saving ...')
-                    
+
                     params = unzip(tparams)
                     np.savez(saveto, history_cost=history_cost, **params)
-                    
+
                     logger.info('Done ...')
 
                 if np.mod(uidx, validFreq) == 0:
                     use_noise.set_value(0.)
-                    
+
                     valid_cost = calu_cost(f_cost, prepare_data_for_cnn, prepare_data_for_rnn, val, kf_valid)
                     history_cost.append([valid_cost])
-                        
+
                     logger.info('Valid {}'.format(np.exp(valid_cost)))
 
         logger.info('Seen {} samples'.format(n_samples))
@@ -229,29 +229,29 @@ def train_model(train, val, test, n_words=21103, img_w=300, max_len=40,
         logger.info('Training interupted')
 
     end_time = time.time()
-    
+
 #    if best_p is not None:
 #        zipp(best_p, tparams)
 #    else:
 #        best_p = unzip(tparams)
-    
-    
+
+
     use_noise.set_value(0.)
     valid_cost = calu_cost(f_cost, prepare_data_for_cnn, prepare_data_for_rnn, val, kf_valid)
     logger.info('Valid {}'.format(np.exp(valid_cost)))
-    
+
     params = unzip(tparams)
     np.savez(saveto, history_cost=history_cost, **params)
 
-    
-    logger.info('The code run for {} epochs, with {} sec/epochs'.format(eidx + 1, 
+
+    logger.info('The code run for {} epochs, with {} sec/epochs'.format(eidx + 1,
                  (end_time - start_time) / (1. * (eidx + 1))))
-    
-    
+
+
     return valid_cost
 
 if __name__ == '__main__':
-    
+
     logger = logging.getLogger('train_autoencoder')
     logger.setLevel(logging.INFO)
     fh = logging.FileHandler('train_autoencoder.log')
@@ -262,17 +262,17 @@ if __name__ == '__main__':
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
     logger.addHandler(fh)
-    
+
     x = cPickle.load(open("./data/bookcorpus_1M.p","rb"))
     train, val, test = x[0], x[1], x[2]
     train_text, val_text, test_text = x[3], x[4], x[5]
     wordtoix, ixtoword = x[6], x[7]
     del x
     del train_text, val_text, test_text
-    
+
     n_words = len(ixtoword)
     ixtoword[n_words] = '<pad_zero>'
     wordtoix['<pad_zero>'] = n_words
     n_words = n_words + 1
-    
+
     valid_cost = train_model(train, val, test, n_words=n_words)
